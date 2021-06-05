fa4038b000029e8d097d5bf3b867763c41a88486
Bantosito <bantos180@gmail.com>
2021-06-05 12:52:41 +0200

    finally working

--- appMain.py
+++ appMain.py
@@ -23,14 +23,21 @@ database = 'reCharge'
 
 mydb = create_engine('mysql+mysqlconnector://' + user+ ' :'+ passw + '@' + host + ':' + str(port) + '/' + database, echo=False)
 
-BBox= (55.7143,55.6228,12.5831,12.6561)
+BBox= (12.5368,12.6484,55.6349,55.7255)
 
-photo = plt.imread("mapCPH.png")
+
+#chargers.to_sql (name="chargingStations",con = mydb, if_exists = 'replace', index=False )
+
+df = pd.read_sql_table("chargingStations",con= mydb)
+df['xAxis'] = df['xAxis'].str.replace(',', '.').astype(float)
+df['yAxis'] = df['yAxis'].str.replace(',', '.').astype(float)
+
+
+photo = plt.imread("map.png")
 fig, ax = plt.subplots(figsize = (8,7))
-#ax.scatter(df.longitude, df.latitude, zorder=1, alpha= 0.2, c='b', s=10)
+ax.scatter(df.xAxis, df.yAxis, zorder=1, alpha= 1, c='r', s=10)
 ax.set_title("DUpaaaa")
-ax.set_xlim(55.7143,55.6228)
-ax.set_ylim(12.5831,12.6561)
+ax.set_xlim(BBox[0],BBox[1])
+ax.set_ylim(BBox[2],BBox[3])
 ax.imshow(photo, zorder=0, extent = BBox, aspect= 'equal')
 plt.show()
-#chargers.to_sql (name="chargingStations",con = mydb, if_exists = 'replace', index=False )
